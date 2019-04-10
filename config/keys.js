6d5e36b26cd2b55eef05aa097cc48b68363e5778
Akshansh Ohm <akshanshohm1999@gmail.com>
2019-04-10 11:50:40 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports={
-    MongoURI:"mongodb+srv://Akshansh05:Akshansh@5@clusterohm-09avm.mongodb.net/test?retryWrites=true"
+    MongoURI:"mongodb+srv://<username>:<password>@clusterohm-09avm.mongodb.net/test?retryWrites=true"
 }
